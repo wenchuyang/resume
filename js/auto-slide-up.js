db85bc505ddaec6ee70054605b1fd35548ddcd4a
wenchuyang <writing_happy@163.com>
2018-07-22 16:30:21 +0800

    将js代码分别放到不同的文件中去

---
+++ js/auto-slide-up.js
@@ -0,0 +1,30 @@
+//添加offset类
+let specialTags = document.querySelectorAll('[data-x]')
+for (let i = 0; i < specialTags.length; i++) {
+  specialTags[i].classList.add('offset')
+}
+
+findClosestAndRemoveOffset()
+window.addEventListener('scroll', function(x){
+  findClosestAndRemoveOffset()
+})
+
+function findClosestAndRemoveOffset() {
+  let specialTags = document.querySelectorAll('[data-x]')
+  let minIndex = 0
+  for (let i = 0; i < specialTags.length; i++) {
+    if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)) {
+      minIndex = i
+    }
+  }
+  //加slideUp动画效果
+  specialTags[minIndex].classList.remove('offset')
+  //找到这个元素之后再通过id找到该元素对应的a标签
+  let id = specialTags[minIndex].id
+  let li = document.querySelector('a[href="#' + id + '"]').parentNode
+  let brotherAndMe = li.parentNode.children
+  for (let i = 0; i < brotherAndMe.length; i++) {
+    brotherAndMe[i].classList.remove('highLight')
+  }
+  li.classList.add('highLight')
+}
---
+++ js/init-swiper.js
@@ -0,0 +1,14 @@
+var mySwiper = new Swiper('.swiper-container', {
+  loop: true,
+
+  // 如果需要分页器
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // 如果需要前进后退按钮
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})        
