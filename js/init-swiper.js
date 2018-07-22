5c015239c7c680138e0e683adeb4d35dc3995fda
wenchuyang <writing_happy@163.com>
2018-07-22 16:41:08 +0800

    模块化之后避免全局变量之间互相影响加上立即执行函数

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,14 +1,16 @@
-var mySwiper = new Swiper('.swiper-container', {
-  loop: true,
-
-  // 如果需要分页器
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // 如果需要前进后退按钮
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-})        
+!function(){
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+  
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })        
+}.call()
--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -1,39 +1,41 @@
-let liTags = document.querySelectorAll('nav.menu > ul > li')
-for (let i = 0; i < liTags.length; i++) {
-  liTags[i].onmouseenter = function (x) {
-    let li = x.currentTarget
-    li.classList.add('active')
-  }
-  liTags[i].onmouseleave = function (x) {
-    let li = x.currentTarget
-    li.classList.remove('active')
-  }
-}
-let aTags = document.querySelectorAll('nav.menu > ul > li > a')
-for (let i = 0; i < aTags.length; i++) {
-  aTags[i].onclick = function (x) {
-    x.preventDefault()
-    let element = document.querySelector(x.currentTarget.getAttribute('href')) /*以这个字符串为选择器来获取元素*/
-    let top = element.offsetTop
-
-    let currentTop = window.scrollY
-    let targetTop = top - 80
-    let t = Math.abs((targetTop - currentTop) / 100 * 250)
-    // Setup the animation loop.
-    function animate(time) {//告诉tween我们是间隔多少毫秒动一次，不需要自己手动指定。
-      //求间隔的API，根据显卡的不同而改变帧率
-      requestAnimationFrame(animate);
-      TWEEN.update(time);
+!function(){
+  let liTags = document.querySelectorAll('nav.menu > ul > li')
+  for (let i = 0; i < liTags.length; i++) {
+    liTags[i].onmouseenter = function (x) {
+      let li = x.currentTarget
+      li.classList.add('active')
     }
-    requestAnimationFrame(animate);
-
-    var coords = { y: currentTop };
-    var tween = new TWEEN.Tween(coords)
-      .to({ y: targetTop }, t)
-      .easing(TWEEN.Easing.Quartic.Out)
-      .onUpdate(function () {
-        window.scrollTo(0, coords.y)
-      })
-      .start();
+    liTags[i].onmouseleave = function (x) {
+      let li = x.currentTarget
+      li.classList.remove('active')
+    }
   }
-}
+  let aTags = document.querySelectorAll('nav.menu > ul > li > a')
+  for (let i = 0; i < aTags.length; i++) {
+    aTags[i].onclick = function (x) {
+      x.preventDefault()
+      let element = document.querySelector(x.currentTarget.getAttribute('href')) /*以这个字符串为选择器来获取元素*/
+      let top = element.offsetTop
+  
+      let currentTop = window.scrollY
+      let targetTop = top - 80
+      let t = Math.abs((targetTop - currentTop) / 100 * 250)
+      // Setup the animation loop.
+      function animate(time) {//告诉tween我们是间隔多少毫秒动一次，不需要自己手动指定。
+        //求间隔的API，根据显卡的不同而改变帧率
+        requestAnimationFrame(animate);
+        TWEEN.update(time);
+      }
+      requestAnimationFrame(animate);
+  
+      var coords = { y: currentTop };
+      var tween = new TWEEN.Tween(coords)
+        .to({ y: targetTop }, t)
+        .easing(TWEEN.Easing.Quartic.Out)
+        .onUpdate(function () {
+          window.scrollTo(0, coords.y)
+        })
+        .start();
+    }
+  }
+}.call()
